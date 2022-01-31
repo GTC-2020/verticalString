5f08486c265526d3dc80c78ce1c54d75f9bea9d3
George <tsurkan.g@gmail.com>
2022-01-31 14:45:03 +0200

    add new variable

--- verticalString.js
+++ verticalString.js
@@ -1,9 +1,8 @@
-
-
 function showVerticalMessage(str){
     const lengthOfOutputString = 10;
+    const firstLatter = "м"
 
-    if(str[0] === "м"){
+    if(str[0] === firstLatter){
         str = str[0].toUpperCase() + str.slice(1);
     }
     for(let i = 0; i < lengthOfOutputString; i++){
