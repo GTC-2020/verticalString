d675ea1144b5ced78315b9a2bce567a3c3336885
George <tsurkan.g@gmail.com>
2022-02-01 10:00:21 +0200

    add variable of lenghth

--- verticalString.js
+++ verticalString.js
@@ -2,7 +2,7 @@ function showVerticalMessage(str){
     const lengthOfOutputString = 10;
     const firstLetter = "м"
 
-    str = str.startsWith(firstLetter) ? str[0].toUpperCase() + str.substr(1, 9):str.substr(0, 10);
+    str = str.startsWith(firstLetter) ? str[0].toUpperCase() + str.substr(1, lengthOfOutputString - 1):str.substr(0, lengthOfOutputString);
 
     for(let char of str){
         console.log(`${char}\n`);
